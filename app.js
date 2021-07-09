32cdb995700981e88d14e92b56e9df123a64eb0e
ncharan999 <charan.nallandala@transformco.com>
2021-07-09 13:40:13 +0530

    added comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!!! Just Started Learning Devops'));
+app.get('/', (req, res) => res.send('Hello World!!! Just Started Learning Devops..and just completed crearting Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
